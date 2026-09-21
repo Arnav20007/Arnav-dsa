b0ed8571020bbb6be44831d4d7f93fbf2b8de07a
Arnav Singh <arnavsinghas1221@gmail.com>
2026-09-22 02:05:40 +0530

    Time: 1 ms (18.46%) | Memory: 8.9 MB (36.93%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('||s[i]== '['||s[i]== '{'){
+        stack<char> st;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            // opening bracket
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
                 st.push(s[i]);
             }
-            else if(s[i] == ')' || s[i] == ']' || s[i] == '}'){
-                if(st.empty())
-    return false;
-            if(s[i] == ')' && st.top() != '(')
-    return false;
-    if(s[i] == ']' && st.top() != '[')
-    return false;
-    if(s[i] == '}' && st.top() != '{')
-    return false;
-    st.pop();
+
+            // closing bracket
+            else {
+                if(st.empty()) return false;
+
+                if(s[i] == ')' && st.top() != '(') return false;
+                if(s[i] == '}' && st.top() != '{') return false;
+                if(s[i] == ']' && st.top() != '[') return false;
+
+                st.pop();
+            }
         }
-        
-        }
-return st.empty();
+
+        return st.empty();
     }
 };
